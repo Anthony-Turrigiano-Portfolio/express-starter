a810cecdb42582fdd037f3a401a1b76b648f55b1
Anthony Turrigiano <tiyper1@gmail.com>
2017-08-24 13:30:57 +0000

    Clean up home.js file in controllers directory

--- controllers/home.js
+++ controllers/home.js
@@ -7,11 +7,7 @@
 module.exports = {
 
     get: ( req, res ) => {
-<<<<<<< HEAD
         res.send('Home Page');    
-=======
-        res.send('Hello World');    
->>>>>>> 02a52eae7ee4685da8616a11cfbe32ac0ea97f05
     }
     
 };
